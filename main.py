d85c88ed4e30a8e5756e58e40b720ee26799dd5c
vcons <vconstantin261@gmail.com>
2022-12-21 01:50:20 +0200

    Added moving file function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import os
 import shutil
 import fisier_extensii
+
+def mutare_fisier(extension, file, path_curent):
+    """
+    Muta fisierele in directorul specific
+    :param extension: determina directorul in care se muta fisierul
+    :param file: fisierul
+    :param path_curent: Locatia initiala
+    :return:
+    """
+    for item in fisier_extensii.extensions():
+        if extension in item.values():
+            shutil.move(fr'{path_curent}\{file}', fr'{path_curent}\{item.keys()}')
