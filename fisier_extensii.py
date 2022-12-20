189e6a50a6ee38887429627a9758f7a855ca2be4
Luai Al-Tajar <luaialtajar1@gmail.com>
2022-12-20 18:57:27 +0200

    added extension file

---
+++ fisier_extensii.py
@@ -0,0 +1,13 @@
+def extensions() -> dict:
+    extension_dictionary = {"excel_extensions": ["XLS", "XLSX", "CSV", "WORD", "DOCX", "DOC", "XLSM"],
+                            "documents_extensions": ["HTML", "HTM", "TXT"],
+                            "rar_extensions": ["RAR", "ZIP", "WINRAR", "7Z"],
+                            "pdf_extensions": ["PDF"],
+                            "torrent_extensions": ["TORRENT"],
+                            "audio_video_extensions": ["WAV", "MP4", "MP3", "MKV", "WEBM"],
+                            "exe_extensions": ["EXE", "MSI"],
+                            "images_extensions": ["PNG", "JPG"],
+                            "ics_extensions": ["ICS"],
+                            "iso_extensions": ["ISO"]}
+
+    return extension_dictionary
