d53c9d03aa2a5cfc4b539ce1af074a2c6267393b
Luai Al-Tajar <luaialtajar1@gmail.com>
2022-12-21 18:06:54 +0200

    Added remaining functions

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import os
 import shutil
 import fisier_extensii
 
+
+def filtru_exstensii(folder_name):
+    """
+    Selecteaza textul de dupa ultimul punct ca si extensie
+    :param folder_name: locatia fisierelor
+    :return:
+    """
+    for file in os.listdir(folder_name):
+        extension = file.split('.')[-1]
+        mutare_fisier(extension, file, folder_name)
+
+
 def mutare_fisier(extension, file, path_curent):
     """
     Muta fisierele in directorul specific
@@ -12,4 +24,24 @@ def mutare_fisier(extension, file, path_curent):
     """
     for item in fisier_extensii.extensions():
         if extension in item.values():
+            creare_director(item.keys(), path_curent)
             shutil.move(fr'{path_curent}\{file}', fr'{path_curent}\{item.keys()}')
+
+
+def creare_director(nume_fisier, loc):
+    """
+    Verifica daca exista un fisier cu numele primit, si creaza unul daca nu exista
+    :param nume_fisier: folosit pentru verificare
+    :param loc: locatia e folosita pentru a crea pathul nou
+    :return:
+    """
+    if os.path.exists(fr'{loc}\{nume_fisier}'):
+        pass
+    else:
+        os.mkdir(nume_fisier)
+
+
+if __name__ == '__main__':
+
+    locatie = input("Locatie:")
+    filtru_exstensii(locatie)
